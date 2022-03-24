529a0f7b61c8c61b258a24e61955c38b6f77fbfd
Vladimir-Pobedinskiy <VladimirPobedinskiy@yandex.ru>
2022-03-25 00:28:35 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import '@/vendor';
+import '@/less/styles.less';
+// import '@/common.blocks/btn/btn.less';
+// import '@/vue/index';
+import '@/common.blocks/index';
+import '@/assets/svg/svg-sprite';
+
